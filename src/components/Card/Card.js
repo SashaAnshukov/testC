a0bf7e67a30137db950c1e7200e264d96124bddc
SashaAnshukov <buenosdias2@yandex.ru>
2023-01-23 00:47:48 +0300

    added state and border

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,41 @@
+import {useState} from 'react';
+
 function Card({card}) {
 
+    const [selectedCard, setSelectedCard] = useState(false);
+    
+    const selectedCardClassName = (
+        `Card__rectangle_border ${selectedCard ? 'Card__rectangle_red' : 'Card__rectangle_border'} `
+    )
+
+    const selectedPointInfoClassName = (
+        `Card__pointInfo ${selectedCard ? 'Card__pointInfo_red' : 'Card__pointInfo'} `
+    )
+
+    function handleClick() {
+        !selectedCard ? setSelectedCard(true) : setSelectedCard(false)
+    }
+    
     return (
         <article className="Card">
-            <div className="Card__rectangle">
-                <span className="Card__corner"></span>
+            <div className={selectedCardClassName} onClick={handleClick}>
+            <div className="Card__rectangle"> 
+                <div className="Card__corner"></div>
                 <p className="Card__description">Сказочное заморское яство</p>
                 <h1 className="Card__title">Нямушка</h1>
                 <h2 className="Card__taste">{card.taste}</h2>
                 <p className="Card__quantity">{card.quantity}</p>
-                <div className="Card__pointInfo">
+                <div className={selectedPointInfoClassName}>
                     <p className="Card__pointInfo_weight">{card.weight}</p>
                     <p className="Card__pointInfo_unit"> кг</p>
                 </div>
+            </div> 
             </div>
-            <span className="Card__text-link">Чего сидишь? Порадуй котэ, купи.</span>
+            <p className="Card__text">
+                {!selectedCard ? 'Чего сидишь? Порадуй котэ, ': card.text}
+                {!selectedCard ? <a className="Card__text_link" href="#" onClick={handleClick}>купи</a> : ''}
+                
+            </p>
         </article>
     );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,21 +8,25 @@ function Main() {
             id: Math.random().toString(16).slice(2),
             taste: 'с фуа-гра', 
             quantity: '10 порций' + '\xa0\xa0\xa0\xa0\xa0' + ' мышь в подарок',
-            weight: '0,5' 
+            weight: '0,5',
+            text: 'Печень утки разварная с артишоками.' 
         }, 
         { 
             id: Math.random().toString(16).slice(2),
             taste: 'с рыбой', 
             quantity: '40 порций' + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + ' 2 мыши в подарок',
-            weight: '2' 
+            weight: '2',
+            text: 'Головы щучьи с чесноком да свежайшая сёмушка.'  
         },
         {
             id: Math.random().toString(16).slice(2),
             taste: 'с курой', 
             quantity: '100 порций' + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + ' 5 мышей в подарок заказчик доволен',
-            weight: '5' 
+            weight: '5',
+            text: 'Филе из цыплят с трюфелями в бульоне.' 
         }
     ];  
+
     
     return (
         <main>
