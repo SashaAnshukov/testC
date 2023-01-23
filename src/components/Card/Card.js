2bebe2375da5dcd911d768f59d0f08d4d129219e
SashaAnshukov <buenosdias2@yandex.ru>
2023-01-23 23:40:50 +0300

    implemented selection logic

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,36 +6,59 @@ function Card({card}) {
     
     const selectedCardClassName = (
         `Card__rectangle_border ${selectedCard ? 'Card__rectangle_red' : 'Card__rectangle_border'} `
-    )
+    );
+
+    const disabledCardClassName = (
+        `Card__rectangle_border ${card.inStock == 0 ? 'Card__rectangle_grey' : 'Card__rectangle_border'}`
+    );
 
     const selectedPointInfoClassName = (
         `Card__pointInfo ${selectedCard ? 'Card__pointInfo_red' : 'Card__pointInfo'} `
-    )
+    );
+
+    const disabledPointInfoClassName = (
+        `Card__pointInfo ${card.inStock == 0 ? 'Card__pointInfo_grey' : 'Card__pointInfo'}`
+    );
+
+    const choosedCardClassName = (card.inStock == 0 ? disabledCardClassName : selectedCardClassName);
+    const choosedPointInfoClassName = (card.inStock == 0 ? disabledPointInfoClassName : selectedPointInfoClassName);
+    const choosedClassName = (card.inStock == 0 ? 'Card__rectangle_disabled' : 'Card__rectangle');
 
     function handleClick() {
         !selectedCard ? setSelectedCard(true) : setSelectedCard(false)
-    }
+    };
     
     return (
         <article className="Card">
-            <div className={selectedCardClassName} onClick={handleClick}>
-            <div className="Card__rectangle"> 
-                <div className="Card__corner"></div>
-                <p className="Card__description">Сказочное заморское яство</p>
-                <h1 className="Card__title">Нямушка</h1>
-                <h2 className="Card__taste">{card.taste}</h2>
-                <p className="Card__quantity">{card.quantity}</p>
-                <div className={selectedPointInfoClassName}>
-                    <p className="Card__pointInfo_weight">{card.weight}</p>
-                    <p className="Card__pointInfo_unit"> кг</p>
-                </div>
-            </div> 
+            <div className={choosedCardClassName} onClick={handleClick}>
+                <div className={choosedClassName}>
+                    <div className='Card__rectangle-cover'> 
+                        <div className="Card__corner"></div>
+                        <p className="Card__description">Сказочное заморское яство</p>
+                        <h1 className="Card__title">Нямушка</h1>
+                        <h2 className="Card__taste">{card.taste}</h2>
+                        <p className="Card__quantity">{card.quantity}</p>
+                    </div> 
+                    <div className={choosedPointInfoClassName}>
+                        <p className="Card__pointInfo_weight">{card.weight}</p>
+                        <p className="Card__pointInfo_unit"> кг</p>
+                    </div>
+                </div> 
             </div>
-            <p className="Card__text">
-                {!selectedCard ? 'Чего сидишь? Порадуй котэ, ': card.text}
-                {!selectedCard ? <a className="Card__text_link" href="#" onClick={handleClick}>купи</a> : ''}
-                
-            </p>
+            <div className="Card__text-info">
+                <p className="Card__text">
+                    {
+                        card.inStock !== 0 
+                        ? 
+                        (!selectedCard ? 'Чего сидишь? Порадуй котэ, ' : card.text)
+                        :
+                        (`Печалька, ${card.textIfTheProductIsOver}`)
+                    }
+                </p>
+                <a className="Card__text_link" href="#" onClick={handleClick}>
+                    {card.inStock !== 0 ? (!selectedCard ? 'купи' : '') :''}
+                </a>
+            </div>
         </article>
     );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,21 +9,27 @@ function Main() {
             taste: 'с фуа-гра', 
             quantity: '10 порций' + '\xa0\xa0\xa0\xa0\xa0' + ' мышь в подарок',
             weight: '0,5',
-            text: 'Печень утки разварная с артишоками.' 
+            text: 'Печень утки разварная с артишоками.',
+            textIfTheProductIsOver: 'с фуа-гра закончился.',
+            inStock: 5,
         }, 
         { 
             id: Math.random().toString(16).slice(2),
             taste: 'с рыбой', 
             quantity: '40 порций' + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + ' 2 мыши в подарок',
             weight: '2',
-            text: 'Головы щучьи с чесноком да свежайшая сёмушка.'  
+            text: 'Головы щучьи с чесноком да свежайшая сёмушка.',
+            textIfTheProductIsOver: 'с рыбой закончился.',
+            inStock: 3,   
         },
         {
             id: Math.random().toString(16).slice(2),
             taste: 'с курой', 
             quantity: '100 порций' + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + ' 5 мышей в подарок заказчик доволен',
             weight: '5',
-            text: 'Филе из цыплят с трюфелями в бульоне.' 
+            text: 'Филе из цыплят с трюфелями в бульоне.',
+            textIfTheProductIsOver: 'с курой закончился.',
+            inStock: 0, 
         }
     ];  
 
